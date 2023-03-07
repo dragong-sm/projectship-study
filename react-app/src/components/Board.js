8fb2f106c91e26330edb74e4d881aabdea5bd637
Sungmin Hong <sm98310@gmail.com>
2023-03-07 21:45:35 +0900

    Day06. 리액트 - TicTacToe 완료
    - 기능 추가
    - css 변경

--- react-app/src/components/Board.js
+++ react-app/src/components/Board.js
@@ -3,33 +3,13 @@ import Square from "./Square";
 import "./Board.css";
 
 // export class Board extends Component {
-const Board = () => {
+const Board = ({ squares, onClick }) => {
   // constructor(props) {
   // super(props);
   // this.state = {
   //   squares: Array(9).fill(null),
   // };
   // }
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  // handleClick(i) {
-  const handleClick = (i) => {
-    // 원본 State를 복사하기
-    // slice 메소드를 사용해서 원본 불변성 지킴
-    // const newSquares = this.state.squares.slice();
-    const newSquares = squares.slice();
-
-    // 클릭한 부분의 square를 X로 변경하기
-    newSquares[i] = "X";
-
-    // 실제 state 업데이트
-    // this.setState({ squares: newSquares });
-    setSquares(newSquares);
-
-    // xIsNext state Toggle
-    setXIsNext(!xIsNext);
-  };
 
   // renderSquare(i) {
   const renderSquare = (i) => {
@@ -39,16 +19,17 @@ const Board = () => {
         // value={this.state.squares[i]}
         value={squares[i]}
         // onClick={() => this.handleClick(i)}
-        onClick={() => handleClick(i)}
+        // onClick={() => handleClick(i)}
+        onClick={() => onClick(i)}
       />
     );
   };
 
   // render() {
-  const status = "Next player : X";
+  // const status = "Next player : X";
   return (
-    <div>
-      <div className="status">{status}</div>
+    <div className="board-wrapper">
+      {/* <div className="status">{status}</div> */}
       <div className="board-row">
         {/* {this.renderSquare(0)}
         {this.renderSquare(1)}
